a7038197d61eb5e9e1e923c92c0839c3772dd904
Vadim Malakhov <malakhovadim@gmail.com>
2017-02-05 16:21:02 -0800

    [Tests]: responses do not contain references to objects.

---
+++ test_data/no_object_refs/results/request.go
@@ -0,0 +1,26 @@
+package vkapiclient
+
+type Request struct {
+	MethodName string
+}
+
+func NewRequest(methodName string) *Request {
+	return nil
+}
+
+func (r *Request) AddIntParam(str string, p int) {
+
+}
+
+func (r *Request) AddStrParam(str string, p string) {
+
+}
+
+func (r *Request) AddBoolParam(str string, p bool) {
+
+}
+
+func (r *Request) Send() []byte {
+	var arr []byte
+	return arr
+}
